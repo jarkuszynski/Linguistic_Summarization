3449ab069ba5d90377eef10a9329c12db054cd92
jarkuszynski <37298865+jarkuszynski@users.noreply.github.com>
2019-06-05 17:05:37 +0200

    initial commit.

---
+++ CsvDataGetter/Model/ParticipantInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvDataGetter.Model
+{
+    public class ParticipantInfo
+    {
+        public string Age { get; set; }
+        public string AgeGroup { get; set; }
+        public string Gender { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ CsvDataGetter/Model/SingleCrimeInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvDataGetter.Model
+{
+    public class SingleCrimeInfo
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string State { get; set; }
+        public string CityOrCounty { get; set; }
+        public int Killed { get; set; }
+        public int Injured { get; set; }
+        public List<GunInfo> GunInfos { get; set; }
+        public List<CharacteristicsInfo> CharacteristicsInfos { get; set; }
+        public List<ParticipantInfo> ParticipantInfos { get; set; }
+    }
+}
